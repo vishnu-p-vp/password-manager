10817bf38ac29ebd7dc03584a6c29de98b25e9e1
vishnu <ctavishnu8@gmail.com>
2026-04-22 10:15:56 +0530

    to aws

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
 from website import create_app
+from flask_cors import CORS
 
 app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
+CORS(app)
--- password-manager-frontend/src/services/API.js
+++ password-manager-frontend/src/services/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // Flask backend URL
+  baseURL: "/api", // Flask backend URL
 });
 
 // Attach token automatically
